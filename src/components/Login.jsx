4b28e352154185089f5f70a9d21a4d5158d5bcc8
Yaseen KC <ymkcmpd@gmail.com>
2024-11-13 11:23:31 +0530

    feat:add sign in/up form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,30 @@
 import Header from "./Header";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { checkValidData } from "../utils/Validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const nameValue = isSignInForm ? "" : name.current?.value;
+    const message = checkValidData(
+      nameValue,
+      email.current?.value,
+      password.current?.value
+    );
+    // console.log(name, email, password);
+    setErrorMessage(message);
+    if (message) return;
+  };
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
+    setErrorMessage(null);
   };
 
   return (
@@ -18,34 +37,44 @@ const Login = () => {
             alt="logo"
           />
         </div>
-        <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg"
+        >
           <h1 className="font-bold text-3xl py-4">
             {isSignInForm ? "Sign In" : "Sign Up"}
           </h1>
           {!isSignInForm && (
             <input
+              ref={name}
               type="text"
               placeholder="Full Name"
               className="p-4 my-4 w-full bg-gray-700"
             />
           )}
           <input
+            ref={email}
             type="text"
             placeholder="Email Address"
             className="p-4 my-4 w-full bg-gray-700"
           />
           <input
+            ref={password}
             type="password"
             placeholder="Password"
             className="p-4 my-4 w-full bg-gray-700"
           />
-          <button className="p-4 my-4 w-full font-bold bg-red-700 rounded-lg cursor-pointer">
+          <p className="text-red-500">{errorMessage}</p>
+          <button
+            className="p-4 my-4 w-full font-bold bg-red-700 rounded-lg cursor-pointer"
+            onClick={handleButtonClick}
+          >
             {isSignInForm ? "Sign In" : "Sign Up"}
           </button>
           <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
             {isSignInForm
-              ? "New to MovieAi? Sign Up Now"
-              : "Already Registered? Sign In Now"}
+              ? "New to MovieAi ? Sign Up Now"
+              : "Already Registered ? Sign In Now"}
           </p>
         </form>
       </div>
---
+++ src/utils/Validate.jsx
@@ -0,0 +1,24 @@
+export const checkValidData = (name, email, password) => {
+  if (name) {
+    const isNameValid = /^[a-zA-Z]+(?:[' -][a-zA-Z]+)*$/.test(name);
+    if (!isNameValid) {
+      return "Name is not valid";
+    }
+  }
+
+  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
+    email
+  );
+
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+  if (!isEmailValid) {
+    return "Email ID is not valid";
+  }
+
+  if (!isPasswordValid) {
+    return "Password is not valid";
+  }
+  return null;
+};
