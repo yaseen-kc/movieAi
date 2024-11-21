bef6e575bacaecde2f50bdd4a43da473ac0fdaad
Yaseen KC <ymkcmpd@gmail.com>
2024-11-21 12:00:59 +0530

    feat(browse): add video background with trailer ID in global state
    Implemented a video background for the browse page using movie trailers.
    Stored the trailer ID in Redux global state for easy access and management.

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useEffect } from "react";
+import { API_OPTIONS } from "./Constants";
+import { useDispatch, useSelector } from "react-redux";
+import { AddTrailerVideo } from "../utils/moviesSlice";
 
-const VideoBackground = () => {
-  return <div>VideoBackground</div>;
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  const dispatch = useDispatch();
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/912649/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    console.log(trailer);
+    dispatch(AddTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerVideo.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
