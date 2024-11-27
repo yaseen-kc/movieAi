68a7e43c0ecf878ed2230d58a551508d1c2804a7
Yaseen KC <ymkcmpd@gmail.com>
2024-11-27 12:49:52 +0530

    feat(search): setup GPT search bar UI
    Designed and implemented the UI for the GPT-powered search bar.
    Prepared the layout and styling for seamless user interaction.

---
+++ src/components/GptSearchPage.jsx
@@ -0,0 +1,17 @@
+import background from "../assets/background-body.jpg";
+import GptMovieSuggestions from "./GptMovieSuggestions";
+import GptSearchBar from "./GptSearchBar";
+
+const GptSearchPage = () => {
+  return (
+    <div>
+      <div className="absolute -z-10">
+        <img src={background} alt="logo" />
+      </div>
+      <GptSearchBar />
+      <GptMovieSuggestions />
+    </div>
+  );
+};
+
+export default GptSearchPage;
--- src/utils/appStore.jsx
+++ src/utils/appStore.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesReducer from "./moviesSlice";
+import gptReducer from "./gptSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userReducer, // Set up user slice in the Redux store
     movies: moviesReducer,
+    gpt: gptReducer,
   },
 });
 
